4136571e5e6a2d1dff307409993d98e0d17090d5
PondiB <pondibrian@gmail.com>
2020-04-13 09:48:28 +0300

    search form choices

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
+
 from .models import Listing
 
 
@@ -7,19 +9,24 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginators = Paginator(listings,6)
     page = request.GET.get('page')
-    paged_listings = paginator.get_page(page)
+    paged_listings = Paginator.get_page(page)
     context = {
         'listings':paged_listings
          }
     return render(request,'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     listing = get_object_or_404(Listing, pk=listing_id)
-    context={
-        'listing':listing
-    }
-    return render(request,'listings/listing.html', context)
+    context = {
+        'listing': listing
+        }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request,'listings/search.html',context)
