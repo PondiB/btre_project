0869fc1618d98c49821c25780b0c9347e8a116f2
PondiB <pondibrian@gmail.com>
2020-04-17 22:19:43 +0300

    dashboard functionality

--- listings/views.py
+++ listings/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
-
 def index(request):
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
-    paginators = Paginator(listings,6)
-    page = request.GET.get('page')
-    paged_listings = Paginator.get_page(page)
-    context = {
-        'listings':paged_listings
-         }
-    return render(request,'listings/listings.html', context)
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+  paginator = Paginator(listings, 6)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
+
+  context = {
+    'listings': paged_listings
+  }
+
+  return render(request, 'listings/listings.html', context)
+
 
 
 def listing(request, listing_id):
